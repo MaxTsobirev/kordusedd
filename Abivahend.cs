8c2d007189e11f35c5c34a5332a5f7956e9024c1
m <t>
2021-10-13 11:22:35 +0300

    5

---
+++ Abivahend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kordusedd
+{
+    class Abivahend
+    {
+        public static void Main(string[] arg)
+        {
+            Console.WriteLine(alamprogramm.Korruta(3, 6));
+            double arv1 = 5;
+            double arv2 = 6;
+
+            Double result = alamprogramm.Eval(arv1.ToString() + "+" + arv2.ToString());
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
--- alamprogramm.cs
+++ alamprogramm.cs
@@ -8,9 +8,16 @@ namespace kordusedd
 {
     class alamprogramm
     {
-        static int Korruta(int arv1, int arv2)
+        public static int Korruta(int arv1, int arv2)
         {
             return arv1 * arv2;
+        }
+
+        public static Double Eval(String expression)
+        {
+            System.Data.DataTable table = new System.Data.DataTable();
+            return Convert.ToDouble(table.Compute(expression, string.Empty));
+
         }
 
         static void Main(string[] args)
@@ -18,6 +25,7 @@ namespace kordusedd
             int a = 2;
             int b = 5;
             Console.WriteLine("{0} * {1} = {2}", a, b, Korruta(a, b));
+            Double result = alamprogramm.Eval("5+6");
             Console.WriteLine(Korruta(3, 6));
             Console.ReadLine();
         }
